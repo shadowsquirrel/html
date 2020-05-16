c3b0ba347329b55e09f70fdf2df5c2e3e12b5631
shadowsquirrel <cnelebi@gmail.com>
2020-05-16 18:35:55 +0200

    new setup many sliders not yet written

--- js/sliderbar.js
+++ js/sliderbar.js
@@ -10,13 +10,15 @@ updatePie = function(a) {
     var data = [{
         values: [y, x],
         labels: ['Opposing Leader', 'Your Leader'],
+        textfont: {
+            color: ['black', 'white'],
+        },
         type: 'pie',
         sort: false,
         hoverinfo: 'percent+label',
         automargin: true,
         marker:{
-            colors: ['rgb(225, 225, 225)', 'rgb(100, 100, 100)']
-            // colors: ['gray', 'yellow']
+            colors: ['rgb(225, 225, 225)', 'rgb(160, 160, 160)']
         }
     }];
 
@@ -27,9 +29,8 @@ updatePie = function(a) {
         font:{
             size: 10
         },
-        margin: {"t": 25, "b": 0, "l": 0, "r": 0},
+        margin: {"t": 0, "b": 0, "l": 0, "r": 0},
         showlegend: false,
-        //paper_bgcolor: 'rgb(255,165,0)'
     };
 
     Plotly.react('pie', data, layout, {displayModeBar: false});
@@ -51,16 +52,11 @@ updateBarHelp = function(a,b) {
         automargin: true,
         showlegend: false,
         marker:{
-            color: ['rgb(150, 150, 255)', 'rgb(200,200,255)'],
-            // color: ['yellow', 'gray'],
-            // line: {
-            //     color: 'rgb(5, 90, 255)',
-            //     width: 1.5
-            // }
-        },
+            color: ['rgb(140, 140, 255)', 'rgb(200,200,255)'],
+                },
         text: [x, y],
-        // textposition: 'outside'
-        textposition: 'auto'
+        textposition: 'outside',
+        cliponaxis: false,
     }];
 
     var layout = {
@@ -68,20 +64,29 @@ updateBarHelp = function(a,b) {
         height: 110,
         width: 150,
         // title: 'Total Help',
-        margin: {"t": 0, "b": 0, "l": 0, "r": 0},
+        margin: {"t": 20, "b": 0, "l": 0, "r": 0},
         yaxis: {
-            fixedrange: false,
+            fixedrange: true,
             autorange: false,
-            range: [0,200]
+            range: [0,400]
+        },
+        xaxis: {
+            fixedrange: true,
+            showline: false,
+            showgrid: false,
+            ticks: '',
+            showticklabels: false,
         },
         bargap: 0.1,
-        // paper_bgcolor: 'rgb(255,165,0)'
-        // plot_bgcolor: 'rgb(180,180,255)'
+        // paper_bgcolor: 'white',
+        // plot_bgcolor: 'white'
     };
 
     Plotly.react('helpBar', data, layout, {displayModeBar: false});
 }
+
 updateBarSabo = function(a, b) {
+    // console.log(a + ', ' + b);
     x = -a;
     y = -b;
     if(typeof(x) === 'undefined') x = 0;
@@ -96,17 +101,11 @@ updateBarSabo = function(a, b) {
         automargin: true,
         showlegend: false,
         marker:{
-            // color: ['rgb(180,180,255)', 'rgb(255,180,180)'],
-            color: ['rgb(255,150,150)', 'rgb(255,200,200)'],
-            // color: ['gray', 'yellow'],
-            // line: {
-            //     color: 'rgb(255, 5, 5)',
-            //     width: 1.5
-            // }
+            color: ['rgb(255,140,140)', 'rgb(255,200,200)'],
         },
         text: [-x, -y],
-        // textposition: 'outside'
-        textposition: 'auto'
+        textposition: 'outside',
+        cliponaxis: false,
     }];
 
     var layout = {
@@ -114,25 +113,28 @@ updateBarSabo = function(a, b) {
         height: 110,
         width: 150,
         // title: 'Total Sabotage',
-        margin: {"t": 0, "b": 0, "l": 0, "r": 0},
+        margin: {"t": 0, "b": 20, "l": 0, "r": 0},
         yaxis: {
-            fixedrange: false,
+            fixedrange: true,
             autorange: false,
-            range: [-200,0]
+            range: [-400,0]
+        },
+        xaxis: {
+            fixedrange: true,
+            showline: false,
+            showgrid: false,
+            ticks: '',
+            showticklabels: false,
         },
         bargap: 0.1,
-        // plot_bgcolor: 'rgb(255,180,180)'
+        // plot_bgcolor: 'white'
     };
 
     Plotly.react('saboBar', data, layout, {displayModeBar: false});
 }
 
 
-
-
-
-
-updateBar = function(a, barId, lighter) {
+updateBar = function(a, barId, lighter, axisOn) {
     var x = a;
     if(typeof(x) === 'undefined') x = 0;
 
@@ -164,14 +166,12 @@ updateBar = function(a, barId, lighter) {
         text: [myLabel],
         textfont: {
             // color: 'rgb(255, 255, 255)'
-            size: '18'
+            size: '14'
         },
         textanchor: 'right',
-        // textposition: 'auto'
         cliponaxis: false,
-        // textposition: myTextPosition
         textposition: 'outside',
-        textpositionsrc: 'x'
+
 
     }];
 
@@ -179,10 +179,9 @@ updateBar = function(a, barId, lighter) {
         barmode: 'group',
         height: 235,//300
         width: 80,
-        // title: 'Total Help',
         margin: {"t": 20, "b": 25, "l": 25, "r": 25},
         yaxis: {
-            fixedrange: false,
+            fixedrange: true,
             autorange: false,
             range: [-100,100],
             layer: 'below traces',
@@ -193,32 +192,114 @@ updateBar = function(a, barId, lighter) {
             },
             tickmode: 'array',
             tickvals: [-100, -50, 0, 50, 100],
-            ticktext: [100, 50, 0, 50, 100]
-
+            ticktext: [100, 50, 0, 50, 100],
+            showline: false,
+            showgrid: axisOn,
+            showticklabels: axisOn,
 
         },
         xaxis: {
             layer: 'below traces',
-            fixedrange: false,
+            fixedrange: true,
             showline: false,
             showgrid: false,
             ticks: '',
             showticklabels: false,
-
         },
-    //     annotations:[{
-    //         align: 'center'
-    //     }]
+
     };
 
     Plotly.react(barId, data, layout, {displayModeBar: false});
 }
 
-updatePie(0.4);
 
 
 
+//VARIABLES AND GRAPHICS INITIATIONS
 
+
+// Follower global variables for both groups
+// your group
+var s1, s2, s3, s4, h1, h2, h3, h4, ts, th;
+s1 = s2 = s3 = s4 = h1 = h2 = h3 = h4 = ts = th = 0;
+// opposing group
+var os1, os2, os3, os4, oh1, oh2, oh3, oh4, ots, oth;
+os1 = os2 = os3 = os4 = oh1 = oh2 = oh3 = oh4 = ots = oth = 0;
+
+// leader global variables
+var efo, efi, efefo, oefo, oefi, oefefo, pwin;
+efo = oefo = 1;//0;
+efi = oefi = 1;
+
+var syncValues = function(hValue, sValue, group){
+    if(group === 'opponent') {
+        oh1 = oh2 = oh3 = oh4 = hValue;
+        os1 = os2 = os3 = os4 = sValue;
+    }
+    if(group === 'our') {
+        // notice s1 and h1 are independent!
+        h2 = h3 = h4 = hValue;
+        s2 = s3 = s4 = sValue;
+    }
+
+}
+
+var syncBars = function(value, group) {
+    if(group === 'opponent') {
+        updateBar(value, 'obar1', 1, false);
+        updateBar(value, 'obar2', 1, false);
+        updateBar(value, 'obar3', 1, false);
+        updateBar(value, 'obar4', 1, false);
+    }
+    if(group === 'our') {
+        updateBar(value, 'bar2', 0, false);
+        updateBar(value, 'bar3', 0, false);
+        updateBar(value, 'bar4', 0, false);
+    }
+}
+
+updateBarAxis = function(barId, axisSwitch) {
+    var update = {
+        'yaxis.showgrid': axisSwitch,
+        'yaxis.showticklabels': axisSwitch
+    };
+    Plotly.relayout(barId, update);
+}
+
+var updateTotal = function() {
+    ts = s1 + s2 + s3 + s4;
+    th = h1 + h2 + h3 + h4;
+    ots = os1 + os2 + os3 + os4;
+    oth = oh1 + oh2 + oh3 + oh4;
+}
+
+var updatePwin = function() {
+    // console.log(th + ', ' + oth + ', ' + ts + ', ' + ots);
+    efi = (1 + th)/(1 + ts);
+    oefi = (1 + oth)/(1 + ots);
+    // console.log(efi + ', ' + oefi);
+    efefo = efi * efo;
+    oefefo = oefi * oefo;
+    pwin = ((efo + oefo) === 0) ? 0.5 : (efefo / (efefo + oefefo));
+}
+
+
+var updateAll = function() {
+    updateTotal();
+    updateBarHelp(th, oth);
+    updateBarSabo(ts, ots);
+
+    updatePwin();
+    updatePie(pwin);
+    // console.log(pwin);
+}
+
+updateAll();
+
+
+// Slider-bar initiations
+
+// YOUR GROUP INITIATION
 var slider1 = document.getElementById('vSlider1');
 var slider2 = document.getElementById('vSlider2');
 var slider3 = document.getElementById('vSlider3');
@@ -227,33 +308,258 @@ var value1 = 0;
 var value2 = 0;
 var value3 = 0;
 var value4 = 0;
-updateBar(value1, 'bar1', 0);
-updateBar(value2, 'bar2', 0);
-updateBar(value3, 'bar3', 1);
-updateBar(value4, 'bar4', 1);
+updateBar(value1, 'bar1', 0, false);
+updateBar(value2, 'bar2', 0, false);
+updateBar(value3, 'bar3', 0, false);
+updateBar(value4, 'bar4', 0, false);
 
+// OPPOSING GROUP INITIATION
+var oslider1 = $('#ovSlider1')[0];
+// var oslider1 = document.getElementById('ovSlider1');
+var oslider2 = document.getElementById('ovSlider2');
+var oslider3 = document.getElementById('ovSlider3');
+var oslider4 = document.getElementById('ovSlider4');
+var ovalue1 = 0;
+var ovalue2 = 0;
+var ovalue3 = 0;
+var ovalue4 = 0;
+updateBar(ovalue1, 'obar1', 1, false);
+updateBar(ovalue2, 'obar2', 1, false);
+updateBar(ovalue3, 'obar3', 1, false);
+updateBar(ovalue4, 'obar4', 1, false);
+
+// YOUR GROUP
+// your decision
 slider1.oninput = function() {
     value1 = parseFloat(slider1.value);
-    updateBar(value1, 'bar1', 0);
-    if(value1>=0) {
-        value1
-    }
+    s1 = value1 >= 0 ? 0 : -value1;
+    h1 = value1 >= 0 ? value1 : 0;
+    updateBar(value1, 'bar1', 0, true);
+    updateAll();
 }
 slider2.oninput = function() {
     value2 = parseFloat(slider2.value);
-    updateBar(value2, 'bar2', 0);
+    // updateBar(value2, 'bar2', 0, true);
+    s2 = value2 >= 0 ? 0 : -value2;
+    h2 = value2 >= 0 ? value2 : 0;
+    syncBars(value2, 'our');
+    updateBarAxis('bar2', true);
+    syncValues(h2, s2, 'our');
+    updateAll();
+    //synching sliders
+    $('#vSlider3, #vSlider4').prop('value', value2);
+    $('#vSlider3, #vSlider4').change();
 }
-
 slider3.oninput = function() {
     value3 = parseFloat(slider3.value);
-    updateBar(value3, 'bar3', 1);
+    // updateBar(value3, 'bar3', 0, true);
+    s3 = value3 >= 0 ? 0 : -value3;
+    h3 = value3 >= 0 ? value3 : 0;
+    syncBars(value3, 'our');
+    updateBarAxis('bar3', true);
+    syncValues(h3, s3, 'our');
+    updateAll();
+    //synching sliders
+    $('#vSlider2, #vSlider4').prop('value', value3);
+    $('#vSlider2, #vSlider4').change();
 }
 slider4.oninput = function() {
     value4 = parseFloat(slider4.value);
-    updateBar(value4, 'bar4', 1);
+    // updateBar(value4, 'bar4', 0, true);
+    s4 = value4 >= 0 ? 0 : -value4;
+    h4 = value4 >= 0 ? value4 : 0;
+    syncBars(value4, 'our');
+    updateBarAxis('bar4', true);
+    syncValues(h4, s4, 'our');
+    updateAll();
+    $('#vSlider2, #vSlider3').prop('value', value4);
+    $('#vSlider2, #vSlider3').change();
+}
+
+// OPPOSING GROUP
+oslider1.oninput = function() {
+    ovalue1 = parseFloat(oslider1.value);
+    // updateBar(ovalue1, 'obar1', 1, true);
+    os1 = ovalue1 >= 0 ? 0 : -ovalue1;
+    oh1 = ovalue1 >= 0 ? ovalue1 : 0;
+    syncBars(ovalue1, 'opponent');
+    updateBarAxis('obar1', true);
+    syncValues(oh1, os1, 'opponent');
+    updateAll();
+    //synching sliders
+    $('#ovSlider2, #ovSlider3, #ovSlider4').prop('value', ovalue1);
+    $('#ovSlider2, #ovSlider3, #ovSlider4').change();
+}
+oslider2.oninput = function() {
+    ovalue2 = parseFloat(oslider2.value);
+    // updateBar(ovalue2, 'obar2', 1, true);
+    os2 = ovalue2 >= 0 ? 0 : -ovalue2;
+    oh2 = ovalue2 >= 0 ? ovalue2 : 0;
+    syncBars(ovalue2, 'opponent');
+    updateBarAxis('obar2', true);
+    syncValues(oh2, os2, 'opponent');
+    updateAll();
+    //synching sliders
+    $('#ovSlider1, #ovSlider3, #ovSlider4').prop('value', ovalue2);
+    $('#ovSlider1, #ovSlider3, #ovSlider4').change();
+}
+oslider3.oninput = function() {
+    ovalue3 = parseFloat(oslider3.value);
+    // updateBar(ovalue3, 'obar3', 1, true);
+    os3 = ovalue3 >= 0 ? 0 : -ovalue3;
+    oh3 = ovalue3 >= 0 ? ovalue3 : 0;
+    syncBars(ovalue3, 'opponent');
+    updateBarAxis('obar3', true);
+    syncValues(oh3, os3, 'opponent');
+    updateAll();
+    //synching sliders
+    $('#ovSlider1, #ovSlider2, #ovSlider4').prop('value', ovalue3);
+    $('#ovSlider1, #ovSlider2, #ovSlider4').change();
+}
+oslider4.oninput = function() {
+    ovalue4 = parseFloat(oslider4.value);
+    // updateBar(ovalue4, 'obar4', 1, true);
+    os4 = ovalue4 >= 0 ? 0 : -ovalue4;
+    oh4 = ovalue4 >= 0 ? ovalue4 : 0;
+    syncBars(ovalue4, 'opponent');
+    updateBarAxis('obar4', true);
+    syncValues(oh4, os4, 'opponent');
+    updateAll();
+    //synching sliders
+    $('#ovSlider1, #ovSlider2, #ovSlider3').prop('value', ovalue4);
+    $('#ovSlider1, #ovSlider2, #ovSlider3').change();
 }
 
 
+// HOVER EFFECT FOR XAXIS SHOWING UP
 
-updateBarHelp(0, 0);
-updateBarSabo(89, 200);
+$('#vSlider1').hover(
+    function() {
+        value1 = parseFloat(slider1.value);
+        setTimeout("updateBarAxis('bar1', true)", 750);
+    },
+    function() {
+        value1 = parseFloat(slider1.value);
+        setTimeout("updateBarAxis('bar1', false)", 1000);
+    }
+);
+$('#vSlider2').hover(
+    function() {
+        value2 = parseFloat(slider2.value);
+        setTimeout("updateBarAxis('bar2', true)", 750);
+        $('#vSlider2, #vSlider3, #vSlider4')
+        .css({'background':'red', 'opacity':'1', 'margin-left': '-50px'});
+        $('#vSlider2, #vSlider3, #vSlider4').addClass('newSlider');
+    },
+    function() {
+        value2 = parseFloat(slider2.value);
+        setTimeout("updateBarAxis('bar2', false)", 1000);
+        // setTimeout("updateBarAxis('bar3', false)", 1000);
+        // setTimeout("updateBarAxis('bar4', false)", 1000);
+        $('#vSlider2, #vSlider3, #vSlider4')
+        .css({'background':'gray', 'opacity':'0.3', 'margin-left': '-60px'});
+        $('#vSlider2, #vSlider3, #vSlider4').removeClass('newSlider');
+    }
+);
+$('#vSlider3').hover(
+    function() {
+        value3 = parseFloat(slider3.value);
+        setTimeout("updateBarAxis('bar3', true)", 750);
+        $('#vSlider2, #vSlider3, #vSlider4')
+        .css({'background':'red', 'opacity':'1', 'margin-left': '-50px'});
+        $('#vSlider2, #vSlider3, #vSlider4').addClass('newSlider');
+    },
+    function() {
+        value3 = parseFloat(slider3.value);
+        // setTimeout("updateBarAxis('bar2', false)", 1000);
+        setTimeout("updateBarAxis('bar3', false)", 1000);
+        // setTimeout("updateBarAxis('bar4', false)", 1000);
+        $('#vSlider2, #vSlider3, #vSlider4')
+        .css({'background':'gray', 'opacity':'0.3', 'margin-left': '-60px'});
+        $('#vSlider2, #vSlider3, #vSlider4').removeClass('newSlider');
+    }
+);
+$('#vSlider4').hover(
+    function() {
+        value4 = parseFloat(slider4.value);
+        setTimeout("updateBarAxis('bar4', true)", 750);
+        $('#vSlider2, #vSlider3, #vSlider4')
+        .css({'background':'red', 'opacity':'1', 'margin-left': '-50px'});
+        $('#vSlider2, #vSlider3, #vSlider4').addClass('newSlider');
+    },
+    function() {
+        value4 = parseFloat(slider4.value);
+        // setTimeout("updateBarAxis('bar2', false)", 1000);
+        // setTimeout("updateBarAxis('bar3', false)", 1000);
+        setTimeout("updateBarAxis('bar4', false)", 1000);
+        $('#vSlider2, #vSlider3, #vSlider4')
+        .css({'background':'gray', 'opacity':'0.3', 'margin-left': '-60px'});
+        $('#vSlider2, #vSlider3, #vSlider4').removeClass('newSlider');
+    }
+);
+
+
+$('#ovSlider1').hover(
+    function() {
+        ovalue1 = parseFloat(oslider1.value);
+        setTimeout("updateBarAxis('obar1', true)", 750);
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4')
+        .css({'background':'red', 'opacity':'1', 'margin-left': '-50px'});
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4').addClass('newSlider');
+    },
+    function() {
+        ovalue1 = parseFloat(oslider1.value);
+        setTimeout("updateBarAxis('obar1', false)", 1000);
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4')
+        .css({'background':'gray', 'opacity':'0.3', 'margin-left': '-60px'});
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4').removeClass('newSlider');
+    }
+);
+$('#ovSlider2').hover(
+    function() {
+        ovalue2 = parseFloat(oslider2.value);
+        setTimeout("updateBarAxis('obar2', true)", 750);
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4')
+        .css({'background':'red', 'opacity':'1', 'margin-left': '-50px'});
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4').addClass('newSlider');
+    },
+    function() {
+        ovalue2 = parseFloat(oslider2.value);
+        setTimeout("updateBarAxis('obar2', false)", 1000);
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4')
+        .css({'background':'gray', 'opacity':'0.3', 'margin-left': '-60px'});
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4').removeClass('newSlider');
+    }
+);
+$('#ovSlider3').hover(
+    function() {
+        ovalue3 = parseFloat(oslider3.value);
+        setTimeout("updateBarAxis('obar3', true)", 750);
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4')
+        .css({'background':'red', 'opacity':'1', 'margin-left': '-50px'});
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4').addClass('newSlider');
+    },
+    function() {
+        ovalue3 = parseFloat(oslider3.value);
+        setTimeout("updateBarAxis('obar3', false)", 1000);
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4')
+        .css({'background':'gray', 'opacity':'0.3', 'margin-left': '-60px'});
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4').removeClass('newSlider');
+    }
+);
+$('#ovSlider4').hover(
+    function() {
+        ovalue4 = parseFloat(oslider4.value);
+        setTimeout("updateBarAxis('obar4', true)", 750);
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4')
+        .css({'background':'red', 'opacity':'1', 'margin-left': '-50px'});
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4').addClass('newSlider');
+    },
+    function() {
+        ovalue4 = parseFloat(oslider4.value);
+        setTimeout("updateBarAxis('obar4', false)", 1000);
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4')
+        .css({'background':'gray', 'opacity':'0.3', 'margin-left': '-60px'});
+        $('#ovSlider1, #ovSlider2, #ovSlider3, #ovSlider4').removeClass('newSlider');
+    }
+);
