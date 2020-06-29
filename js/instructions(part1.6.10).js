263c99463450aa4fae6e143d1e9b0574775fdf48
shadowsquirrel <cnelebi@gmail.com>
2020-06-29 18:14:31 +0200

    minor changes

--- js/instructions(part1.6.10).js
+++ js/instructions(part1.6.10).js
@@ -1,9 +1,9 @@
-$('.f1,.f2,.f3,.f4').css({'opacity':'1'});
-$('.continueButton').css({'opacity':'1'});
-$('.b1, .b2, .l34').css({'border':'5px solid rosybrown', 'opacity':'0.4'});
-$('.b4, .b3').css({'border':'5px solid lime', 'opacity':'1'});
-$('.1a2, .2a3').css({'opacity':'0.5'});
-$('.3aw, .w34, .wa5, .5a6, .b5, .b6, .4a5, .5a6')
-.css({'opacity':'0', 'border':'solid 3px rgb(160, 160, 160)'});
-$('.l34').css({'border':'5px solid darkkhaki', 'opacity':'0.7'});
-$('.3al, .la4')
+// $('.f1,.f2,.f3,.f4').css({'opacity':'1'});
+// $('.continueButton').css({'opacity':'1'});
+// $('.b1, .b2, .l34').css({'border':'5px solid rosybrown', 'opacity':'0.4'});
+// $('.b4, .b3').css({'border':'5px solid lime', 'opacity':'1'});
+// $('.1a2, .2a3').css({'opacity':'0.5'});
+// $('.3aw, .w34, .wa5, .5a6, .b5, .b6, .4a5, .5a6')
+// .css({'opacity':'0', 'border':'solid 3px rgb(160, 160, 160)'});
+// $('.l34').css({'border':'5px solid darkkhaki', 'opacity':'0.7'});
+// $('.3al, .la4')
